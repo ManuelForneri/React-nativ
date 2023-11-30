7b2424193754fb5c815415806d494f67e8483dc7
ManuelForneri <manuelforneri@hotmail.com>
2023-11-29 22:17:56 -0300

    Add Crud for add products

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
+import { useState } from "react";
 import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [titleProduct, setTitleProduct] = useState("");
+  const [priceProduct, setPriceProduct] = useState("");
+  const [onModal, setOnModal] = useState(false);
+
+  const handlerAddProducts = () => {
+    const newProduct = {
+      id: Date.now(),
+      price: priceProduct,
+      title: titleProduct,
+    };
+    setProducts((current) => [...current, newProduct]);
+    setTitleProduct("");
+    setPriceProduct("");
+    console.log(products);
+  };
+  const openDeletedModal = () => {};
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          style={styles.input}
+          placeholder="Producto"
+          onChangeText={(t) => setTitleProduct(t)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Precio"
+          onChangeText={(t) => setPriceProduct(t)}
+        />
+        <Button title="ADD" onPress={handlerAddProducts} />
       </View>
       <View style={styles.listContainer}>
-        <View style={styles.cardProduct}>
-          <Text>Coca cola</Text>
-          <Text>$1000</Text>
-          <Button title="Del" />
-        </View>
+        {products.map((prod) => (
+          <View key={prod.id} style={styles.cardProduct}>
+            <Text>{prod.title}</Text>
+            <Text>${prod.price}</Text>
+            <Button title="Del" onPress={openDeletedModal} />
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -26,11 +56,12 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
   input: {
-    width: "70%",
+    width: "30%",
     borderWidth: 1,
     padding: 4,
   },
   inputContainer: {
+    gap: 10,
     width: "100%",
     flexDirection: "row",
     marginBottom: 20,
