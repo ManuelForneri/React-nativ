d8d5e74ac0d116abdc700d5cae0af7c604747d7a
ManuelForneri <manuelforneri@hotmail.com>
2023-12-22 12:49:07 -0300

    Create Navigator.js

---
+++ src/Navigation/Navigator.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeBar from "../Components/HomeBar";
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouterName="Home"
+        screenOptions={({ route }) => {
+          return {
+            header: () => {
+              return <HomeBar />;
+            },
+          };
+        }}
+      >
+        <Stack.Screen />
+        <Stack.Screen />
+        <Stack.Screen />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigator;
+
+const styles = StyleSheet.create({});
